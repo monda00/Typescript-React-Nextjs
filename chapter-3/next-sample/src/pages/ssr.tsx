77c1f313d0fee8f5b1a62537990aa96b6ba08e51
monda00 <monda0524@gmail.com>
2023-04-03 21:22:11 +0900

    add ssr

---
+++ chapter-3/next-sample/src/pages/ssr.tsx
@@ -0,0 +1,39 @@
+import { GetServerSideProps, NextPage } from "next"
+import Head from "next/head"
+
+type SSRProps = {
+  message: string
+}
+
+const SSR: NextPage<SSRProps> = (props) => {
+  const { message } = props
+
+  return (
+    <div>
+      <Head>
+        <title> Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <p>This page is redered when client access to Server using SSR. </p>
+        <p>{message}</p>
+      </main>
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<SSRProps> = async (
+  context
+) => {
+  const timestamp = new Date().toLocaleString()
+  const message = `Client access at ${timestamp}`
+  console.log(message)
+
+  return {
+    props: {
+      message
+    }
+  }
+}
+
+export default SSR
